ae22b7c0924a78d71107b43e27e959b654605a43
Jack <jk19011999@gmail.com>
2023-11-17 19:52:50 +0530

    Created footer component

--- src/components/ConnectUs/Footer.js
+++ src/components/ConnectUs/Footer.js
@@ -1,9 +1,33 @@
 import './footer.css'
 import React from 'react'
+import { FaGithub, FaLinkedin, FaInstagram, FaYoutube, FaCopyright } from 'react-icons/fa'
 
 export default function Footer() {
     return (
-        <div className='footer-container'>
+        <div className='footerpage'>
+            <div className='footer-overlay'>
+                <div className="container footer-container">
+                    <a href="https://github.com/Xaptured" target="_blank" rel="noreferrer">
+                        <FaGithub className='icon' />
+                    </a>
+                    <span className="icon-space"></span>
+                    <a href="https://github.com/Xaptured" target="_blank" rel="noreferrer">
+                        <FaLinkedin className='icon' />
+                    </a>
+                    <span className="icon-space"></span>
+                    <a href="https://www.instagram.com/xaptured__007/" target="_blank" rel="noreferrer">
+                        <FaInstagram className='icon' />
+                    </a>
+                    <span className="icon-space"></span>
+                    <a href="https://www.youtube.com/channel/UCDKMpLyKzruI-q_aXS5l_Uw" target="_blank" rel="noreferrer">
+                        <FaYoutube className='icon' />
+                    </a>
+                </div>
+                <div className='copyright_container'>
+                    <FaCopyright className='copyright_icon' />
+                    <span className='copyright_content'>Copyright 2023. All rigts reserved by TheJackFolio. Created by Jack with Love.</span>
+                </div>
+            </div>
         </div>
     )
 }
