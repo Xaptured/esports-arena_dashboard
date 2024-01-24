8edd7105c2a8268cc8b4814115176fa1dbc7955a
Jack <jk19011999@gmail.com>
2024-01-25 01:40:44 +0530

    Fix

--- src/components/Participant/ParticipantContainer.js
+++ src/components/Participant/ParticipantContainer.js
@@ -27,6 +27,7 @@ export default function ParticipantContainer() {
     }
 
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         isProfileCompleted();
     }, [])
 
