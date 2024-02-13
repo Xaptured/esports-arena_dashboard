1018f940bc154e864383a74a166b1e3ff2242cb4
Jack <jk19011999@gmail.com>
2024-02-13 21:21:16 +0530

    Added create a team

--- src/components/Common/EventDetails.js
+++ src/components/Common/EventDetails.js
@@ -1,21 +1,141 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './eventdetails.css'
 import { useAtomValue } from 'jotai';
 import { eventDetailsAtom, eventDetailsAtomCopy } from '../../atoms/eventAtom';
 import { useCopyValueAtom } from '../../atoms/loginDataAtom';
+import { RxCross2 } from "react-icons/rx";
+import backendService from '../../services/backendService';
+import { loggedInUserAtom, loggedInUserAtomCopy } from '../../atoms/loginDataAtom';
 
 export default function EventDetails() {
 
     // ESA-058-START
     const useCopyAtom = useAtomValue(useCopyValueAtom);
     let eventDetailsAtomResult;
+    let loggedInUserAtomResult;
     if (useCopyAtom) {
         eventDetailsAtomResult = eventDetailsAtomCopy;
+        loggedInUserAtomResult = loggedInUserAtomCopy;
     } else {
         eventDetailsAtomResult = eventDetailsAtom;
+        loggedInUserAtomResult = loggedInUserAtom;
     }
     // ESA-058-END
     const eventDetails = useAtomValue(eventDetailsAtomResult);
+    const [showJoinForm, setShowJoinForm] = useState(false);
+    const [showCreateForm, setShowCreateForm] = useState(false);
+    const [inputFields, setInputFields] = useState(['']);
+    const [errorMsg, setErrorMsg] = useState('');
+    const [countPlayers, setCountPlayers] = useState(1);
+    const [teamName, setTeamName] = useState(null);
+    const [eventId, setEventId] = useState(null);
+    const [disableSubmit, setDisableSubmit] = useState(false);
+    const [isRegistered, setRegistered] = useState(false);
+    const [message, setMessage] = useState(null);
+    const loggedInUser = useAtomValue(loggedInUserAtomResult);
+
+    const isFieldUnique = (value) => {
+        const subarray = inputFields.slice(0, inputFields.length - 1);
+        return subarray.includes(value);
+    };
+
+    const handleAddField = () => {
+        const val = inputFields[inputFields.length - 1];
+        if (val === '') {
+            setErrorMsg("Please fill the email.");
+        } else {
+            if (!isFieldUnique(val)) {
+                setErrorMsg('');
+                let counter = countPlayers;
+                setCountPlayers(++counter);
+                setInputFields([...inputFields, '']);
+            } else {
+                setErrorMsg('Please enter a new email address.');
+            }
+        }
+    };
+
+    const handleCancelField = () => {
+        if (inputFields.length > 1) {
+            let counter = countPlayers;
+            setCountPlayers(--counter);
+            const updatedFields = [...inputFields];
+            updatedFields.pop();
+            setInputFields(updatedFields);
+        }
+    };
+
+    const handleInputChangeTeamName = (e) => {
+        setTeamName(e.target.value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const val = inputFields[inputFields.length - 1];
+        if (val === '') {
+            setErrorMsg("Please fill the email.");
+        } else {
+            if (!isFieldUnique(val)) {
+                setErrorMsg('');
+                setDisableSubmit(true);
+                const emailArr = inputFields.map((inputEmail) => ({ email: inputEmail, playerNumber: null }));
+                const teamStatus = eventDetails.type === 'FREE' ? 'FREE' : 'PENDING';
+                const payload = {
+                    name: teamName,
+                    eventId: eventId,
+                    teamStatus: teamStatus,
+                    detail: emailArr,
+                    message: null
+                };
+                const result = await backendService.saveTeam(payload);
+                if (result.message === 'Request Processed') {
+                    const message = eventDetails.type === 'FREE' ? 'Team successfully registered' : 'Team successfully registered and it is in pending state. Once organizer approves it, you will receive an email of confirmation.';
+                    setMessage(message);
+                    setRegistered(true);
+                } else {
+                    setErrorMsg('Error occurred. Please try again later.');
+                    setDisableSubmit(false);
+                }
+
+            } else {
+                setErrorMsg('Please enter a new email address.');
+            }
+        }
+    }
+
+    const handleInputChange = (index, event) => {
+        const values = [...inputFields];
+        values[index] = event.target.value;
+        setInputFields(values);
+    };
+
+    const handleJoinTeamClick = () => {
+        setShowJoinForm(!showJoinForm);
+    }
+
+    const handleCreateTeamClick = () => {
+        setShowCreateForm(!showCreateForm);
+    }
+
+    const getEventId = async () => {
+        // ESA-058: change the team name to get from eventDetails atom
+        const response = await backendService.getEventId('PUBG-EVENT');
+        setEventId(response);
+    }
+
+    const isRegisteredInEvent = async () => {
+        const response = await backendService.isRegistered(eventId, eventDetails.name, loggedInUser.email);
+        if (response) {
+            const message = eventDetails.type === 'FREE' ? 'Team successfully registered' : 'Team successfully registered and it is in pending state. Once organizer approves it, you will receive an email of confirmation.';
+            setMessage(message);
+        }
+        setRegistered(response);
+    }
+
+    useEffect(() => {
+        getEventId();
+        isRegisteredInEvent();
+    }, [])
     return (
         <>
             {
@@ -65,14 +185,85 @@ export default function EventDetails() {
                             }
                         </ul>
                     </div>
-                    <div className="event-forms">
-                        <button type="button" className='btn btn-outline-light button_team'>
-                            JOIN A TEAM
-                        </button>
-                        <button type="button" className='btn btn-outline-light button_team'>
-                            CREATE A TEAM
-                        </button>
-                    </div>
+                    {
+                        !isRegistered ?
+                            <div className="event-forms">
+                                {
+                                    !showJoinForm ?
+                                        <button type="button" className='btn btn-outline-light button_team' onClick={handleJoinTeamClick}>
+                                            JOIN A TEAM
+                                        </button>
+                                        :
+                                        <div>
+                                            Join Form
+                                        </div>
+                                }
+                                {
+                                    !showCreateForm ?
+                                        <button type="button" className='btn btn-outline-light button_team' onClick={handleCreateTeamClick}>
+                                            CREATE A TEAM
+                                        </button>
+                                        :
+                                        <div className='create-team-form'>
+                                            <div className="form-box-create-team">
+                                                <form onSubmit={handleSubmit}>
+                                                    <div className="close-icon-container">
+                                                        <RxCross2 size={30} color="grey" onClick={handleCreateTeamClick} />
+                                                    </div>
+
+                                                    <h1>Create a team</h1>
+                                                    <div className='mb-3 input-box'>
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Team name"
+                                                            value={teamName}
+                                                            onChange={handleInputChangeTeamName}
+                                                            required />
+                                                        <i className='bx bxs-user'></i>
+                                                    </div>
+                                                    <i className='bx bxs-user'></i>
+                                                    {inputFields.map((inputField, index) => (
+                                                        <div key={index} className='mb-3 input-box'>
+                                                            <input
+                                                                type="email"
+                                                                placeholder="Email"
+                                                                value={inputField}
+                                                                onChange={(event) => handleInputChange(index, event)}
+                                                                required
+                                                            />
+                                                            <i className='bx bxs-user'></i>
+                                                        </div>
+                                                    ))}
+                                                    <div className="add-more">
+                                                        {
+                                                            countPlayers < eventDetails.playersPerSlot ?
+                                                                <label onClick={handleAddField} style={{ marginRight: '4%' }}>+Add more</label>
+                                                                :
+                                                                ''
+                                                        }
+                                                        {
+                                                            countPlayers > 1 ?
+                                                                <label onClick={handleCancelField}>-Cancel</label>
+                                                                :
+                                                                ''
+                                                        }
+                                                    </div>
+                                                    <div className="action-bar">
+                                                        <button type="submit" className='btn btn-outline-light button_submit_create_team' disabled={disableSubmit}>
+                                                            Submit
+                                                        </button>
+                                                    </div>
+                                                    {/* Use this space to show error messages */}
+                                                    <div className="message-space">{errorMsg}</div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                }
+
+                            </div>
+                            :
+                            <div>{message}</div>
+                    }
                 </div>
             }
         </>
--- src/services/backendService.js
+++ src/services/backendService.js
@@ -138,7 +138,51 @@ const backendService = {
         } catch (error) {
             console.log("Error while getting event details with name", error);
         }
-    }
+    },
+
+    async getEventId(eventName) {
+        try {
+            const url = 'http://localhost:8086/events/get-event-id/' + eventName;
+            const result = await fetch(url);
+
+            const jsonResult = await result.json();
+
+            return jsonResult;
+        } catch (error) {
+            console.log("Error while getting event id with name", error);
+        }
+    },
+
+    async saveTeam(team) {
+        try {
+            const result = await fetch('http://localhost:8086/events/save-team?isCreate=true&isUpdate=false', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(team)
+            });
+
+            const jsonResult = await result.json();
+
+            return jsonResult;
+        } catch (error) {
+            console.log("Error while saving credential data", error);
+        }
+    },
+
+    async isRegistered(eventId, eventName, email) {
+        try {
+            const url = 'http://localhost:8086/events/is-registered?eventId=' + eventId + '&eventName=' + eventName + '&email=' + email;
+            const result = await fetch(url);
+
+            const jsonResult = await result.json();
+
+            return jsonResult;
+        } catch (error) {
+            console.log("Error while getting isRegistered", error);
+        }
+    },
 }
 
 export default backendService;
