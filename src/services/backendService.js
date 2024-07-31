913fcd5b0f7e20427f22d30b383b955ae616f732
Jack <jk19011999@gmail.com>
2024-08-01 01:08:52 +0530

    Fixed text when Organizer approves a team

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -197,6 +197,19 @@ const backendService = {
         }
     },
 
+    async getTeamWithEventIDAndEmail(eventId, eventName, email) {
+        try {
+            const url = 'http://localhost:8086/events/get-team-for-event?eventId=' + eventId + '&eventName=' + eventName + '&email=' + email;
+            const result = await fetch(url);
+
+            const jsonResult = await result.json();
+
+            return jsonResult;
+        } catch (error) {
+            console.log("Error while getting getTeamDetailsForEvent", error);
+        }
+    },
+
     async remainingPlayersPerSlotCount(eventId, eventName, email) {
         try {
             const url = 'http://localhost:8086/events/get-remaining-players-per-slot?eventId=' + eventId + '&eventName=' + eventName + '&email=' + email;
