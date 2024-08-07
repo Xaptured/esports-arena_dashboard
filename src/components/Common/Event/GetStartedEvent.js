d9f39f1e867f657ec01b51fa6158f93c17b20ca8
Jack <jk19011999@gmail.com>
2024-08-08 00:58:37 +0530

    Modified styles and content

--- src/components/Common/Event/GetStartedEvent.js
+++ src/components/Common/Event/GetStartedEvent.js
@@ -9,16 +9,23 @@ export default function GetStartedEvent(props) {
     }
     return (
         <div style={{ marginTop: "18%" }}>
-            {
-                userType === "participant" ? 'Start your journey by participating in events.' : ''
-            }
             <br />
-            <p style={{ fontSize: '30px', color: 'grey', marginTop: '1%' }}>
-                Click on any event to see description and rules.
-            </p>
+            {
+                userType === "participant" && <div>
+                    Start your journey by participating in events.
+                    <p style={{ fontSize: '30px', color: 'grey', marginTop: '1%' }}>
+                        Click on any event to see description and rules.
+                    </p>
+                </div>
+
+            }
+
             {
                 userType === "participant" ? ''
                     : <>
+                        <p style={{ fontSize: '30px', color: 'grey', marginTop: '1%' }}>
+                            Click on any event to see description and rules.
+                        </p>
                         <p style={{ marginBottom: '5%' }}>or Create a new event</p>
                         <div className='wrapper-create-event'>
                             <button type="button" className='btn btn-outline-light button-create-event' onClick={showForm}>
